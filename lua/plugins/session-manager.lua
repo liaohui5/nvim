0a0e0e694f3fc5e2e3b2a277afebef1c838352e9
secret <secretx500@qq.com>
2024-06-24 14:45:30 +0800

    :sparkles: feat: remote markdown language support

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -1,40 +1,34 @@
 return {
   { "persistence.nvim", enabled = false },
+  { "nvim-lua/plenary.nvim", enabled = true },
   {
     -- use neovim session manager replace persistence
     "Shatur/neovim-session-manager",
     enabled = true,
     event = "VeryLazy",
     dependencies = {
-      {
-        "nvim-lua/plenary.nvim",
-      },
-      {
-        "folke/which-key.nvim",
-        opts = {
-          defaults = {
-            ["<leader>q"] = {
-              name = "+quit", -- replace "quit/session"
-            },
-            ["<leader>p"] = {
-              name = "+session",
-            },
+      "folke/which-key.nvim",
+      opts = {
+        defaults = {
+          ["<leader>q"] = {
+            name = "+quit", -- replace "quit/session"
+          },
+          ["<leader>p"] = {
+            name = "+session",
           },
         },
       },
     },
     opts = function(_, opts)
+      local Path = require("plenary.path")
       local autoload_mode = require("session_manager.config").AutoloadMode
-      local sessions_path = vim.fn.stdpath("data") .. "/sessions"
+      local sessions_path = Path:new(vim.fn.stdpath("data"), "sessions")
       return vim.tbl_deep_extend("force", opts, {
         sessions_dir = sessions_path,
         autoload_mode = autoload_mode.Disabled, -- Disabled, CurrentDir, LastSession
-        path_replacer = "__",
-        colon_replacer = "++",
         autosave_last_session = true,
         autosave_ignore_not_normal = true,
         autosave_only_in_session = true,
-        max_path_length = 80,
         autosave_ignore_filetypes = {
           ".DS_Store",
           "gitcommit",
