95bfc6540918a5892003ed97cd56736807f04a37
liaohui5 <liaohui5@foxmail.com>
2024-08-09 02:09:30 +0800

    refactor: refact im-select enabled function

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,26 +1,33 @@
 ---@diagnostic disable: undefined-field
 -- auto switch input method
 -- github: https://github.com/keaising/im-select.nvim
+
+-- get im-select command string once by os_info
+local im_select_command = (function()
+  local os_info = vim.loop.os_uname()
+  local command = "im-select"
+  if string.find(os_info.release, "WSL") ~= nil then
+    command = command .. ".exe"
+  end
+  return command
+end)()
+
 return {
   "keaising/im-select.nvim",
   event = "VeryLazy",
   enabled = function()
     -- disabled im-select in Windows
     local os_info = vim.loop.os_uname()
-    return string.find(os_info.sysname, "Windows") == nil
-  end,
-  opts = function()
-    local os_info = vim.loop.os_uname()
-    local command = "im-select"
-
-    -- enabled im-select in wsl
-    -- must be add im-select.exe to environment PATH
-    if string.find(os_info.release, "WSL") ~= nil then
-      command = command .. ".exe"
+    if string.find(os_info.sysname, "Windows") then
+      return false
     end
 
+    -- check im_select_command is executable
+    return vim.fn.executable(im_select_command)
+  end,
+  opts = function()
     return {
-      default_command = command,
+      default_command = im_select_command,
       set_default_events = {
         "VimEnter",
         "FocusGained",
