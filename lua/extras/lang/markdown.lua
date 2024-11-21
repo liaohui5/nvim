e5430952c075a76c464f51b60bf5da8e36bea83b
secret <secretx500@outlook.com>
2024-11-21 19:08:30 +0800

    refactor(dashboard): :recycle: remove dashboard, use snacks.dashboard

--- lua/extras/lang/markdown.lua
+++ lua/extras/lang/markdown.lua
@@ -1,14 +1,31 @@
 -- https://www.lazyvim.org/extras/lang/markdown
 return {
   {
+    "stevearc/conform.nvim",
+    optional = true,
+  },
+  {
     "williamboman/mason.nvim",
-    opts = {
-      -- not lint markdown syntax
-      ensure_installed = { "markdown-toc" },
-    },
+    opts = { ensure_installed = {} },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = false,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = false,
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     enabled = false,
   },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       marksman = {},
+  --     },
+  --   },
+  -- },
 }
