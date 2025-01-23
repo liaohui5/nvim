da4cc63b7d643221a6a5edd6ae67636dcd3d57a3
secret <secretx500@outlook.com>
2025-01-24 00:41:31 +0800

    refactor(lua/plugins/lspconfig.lua): 禁用默认的<c-k>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,25 +1,30 @@
 return {
   "neovim/nvim-lspconfig",
   event = "VeryLazy",
-  opts = {
-    inlay_hints = {
-      enabled = false,
-    },
-    servers = {
-      emmet_language_server = {
-        -- better html and css support
-        filetypes = {
-          "html",
-          "css",
-          "less",
-          "sass",
-          "scss",
-          "pug",
-          "vue",
-          "javascriptreact",
-          "typescriptreact",
+  opts = function(_, opts)
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "<c-k>", false } -- disabled <c-k>
+
+    return vim.tbl_deep_extend("force", opts, {
+      inlay_hints = {
+        enabled = false,
+      },
+      servers = {
+        emmet_language_server = {
+          -- better html and css support
+          filetypes = {
+            "html",
+            "css",
+            "less",
+            "sass",
+            "scss",
+            "pug",
+            "vue",
+            "javascriptreact",
+            "typescriptreact",
+          },
         },
       },
-    },
-  },
+    })
+  end,
 }
