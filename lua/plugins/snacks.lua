e3c34991a4272ac9dcf7c914220ae6cd68f58ac1
secret <secretx500@outlook.com>
2025-04-08 23:35:49 +0800

    build: update plugin commits and feature flags in configs

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,10 +3,31 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    words = {
-      enabled = true,
+    animate = {
+      enabled = false,
+    },
+    debug = {
+      enabled = false,
+    },
+    dim = {
+      enabled = false,
     },
     notifier = {
+      enabled = false,
+    },
+    profiler = {
+      enabled = false,
+    },
+    scratch = {
+      enabled = false,
+    },
+    scroll = {
+      enabled = false,
+    },
+    words = {
+      enabled = false,
+    },
+    zen = {
       enabled = false,
     },
     indent = {
@@ -14,9 +35,6 @@ return {
       scope = {
         enabled = false,
       },
-    },
-    scroll = {
-      enabled = false,
     },
     terminal = {
       win = {
