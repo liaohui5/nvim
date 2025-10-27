6e1998af048896b8af47f5760b4a786043c5fcb0
secret <secretx500@outlook.com>
2025-10-27 15:33:35 +0800

    feat: update lazyvim to v15.12

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,32 +1,27 @@
 return {
   "neovim/nvim-lspconfig",
   event = "VeryLazy",
-  opts = function(_, opts)
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-    -- disabled default keymaps ctrl+k
-    table.insert(keys, { "<c-k>", false })
-
-    return vim.tbl_deep_extend("force", opts, {
-      inlay_hints = {
-        enabled = false,
-      },
-      servers = {
-        emmet_language_server = {
-          -- better html and css support
-          filetypes = {
-            "html",
-            "css",
-            "less",
-            "sass",
-            "scss",
-            "pug",
-            "vue",
-            "javascriptreact",
-            "typescriptreact",
-          },
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "<c-k>", false }, -- disable default lsp server keymap
         },
       },
-    })
-  end,
+      emmet_language_server = {
+        -- better html and css support
+        filetypes = {
+          "html",
+          "css",
+          "less",
+          "sass",
+          "scss",
+          "pug",
+          "vue",
+          "javascriptreact",
+          "typescriptreact",
+        },
+      },
+    },
+  },
 }
