b7a45b0e2d1c49a7150a50587ed6012a19c11934
secret <secretx500@outlook.com>
2025-10-31 15:53:18 +0800

    build: update commit hashes for various plugins and configurations

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -36,3 +36,21 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
+
+-- show listchars in visual mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = augroup("show_listchars_visual"),
+  pattern = "*:v*",
+  callback = function()
+    vim.opt.list = true
+  end,
+})
+
+-- hide listchars in normal mode
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = augroup("show_listchars_normal"),
+  pattern = "*:n*",
+  callback = function()
+    vim.opt.list = false
+  end,
+})
