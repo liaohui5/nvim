b4228717aa2050964f56a48077ad2b6af0a5f5e5
secret <secretx500@outlook.com>
2025-11-14 20:48:49 +0800

    feat: disable ai plugin kickside nes feature

---
+++ lua/plugins/kickside.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    -- https://github1s.com/folke/sidekick.nvim/blob/HEAD/lua/sidekick/config.lua
+    nes = {
+      enabled = false,
+    },
+  },
+}
