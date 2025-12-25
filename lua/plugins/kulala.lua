5848cf778c6c59091210003fd7c78c8f8abc910a
secret <secretx500@qq.com>
2025-12-26 00:13:31 +0800

    :recycle: refactor: optimize kulala ui config

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -5,6 +5,42 @@ return {
     "http",
     "rest",
   },
+  opts = {
+    ui = {
+      show_icons = "signcolumn",
+      disable_news_popup = true,
+      default_view = "headers_body",
+      default_winbar_panes = {
+        "headers_body",
+        -- "body",
+        -- "headers",
+        "script_output",
+        "stats",
+        "verbose",
+        "report",
+        -- "help",
+      },
+      winbar_labels = {
+        -- body = "Body",
+        -- headers = "Headers",
+        -- headers_body = "All",
+        -- verbose = "Verbose",
+        script_output = "Output",
+        -- stats = "Stats",
+        -- report = "Report",
+        -- help = "Help",
+      },
+    },
+
+    kulala_keymaps = {
+      ["Show verbose"] = {
+        "C",
+        function()
+          require("kulala.ui").show_verbose()
+        end,
+      },
+    },
+  },
   keys = {
     {
       "<c-enter>",
